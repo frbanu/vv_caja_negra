ddc315e06e89b722cc0ea1760447b5b3d8f2cdce
Teo Roca <teo.roca798@gmail.com>
2021-11-09 19:02:27 +0100

    isSubList tests

---
+++ src/test/java/com/practica/cajanegra/IsSublistTests.java
@@ -0,0 +1,42 @@
+package com.practica.cajanegra;
+import com.cajanegra.SingleLinkedListImpl;
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IsSublistTests {
+    private static SingleLinkedListImpl<Character> emptyList;
+    private static SingleLinkedListImpl<Character>  filledList;
+    private static SingleLinkedListImpl<Character> equalFilledSublist;
+    private static SingleLinkedListImpl<Character> differentFilledSublist;
+    private static SingleLinkedListImpl<Character> shortSublist;
+    private static SingleLinkedListImpl<Character> emptySublist;
+    @BeforeAll
+    public static void setUp() {
+        emptyList = new SingleLinkedListImpl<Character>();
+        filledList = new SingleLinkedListImpl<Character>('A', 'B', 'G', 'Z', 'I', 'K', 'R', 'V');
+        equalFilledSublist = new SingleLinkedListImpl<Character>('A', 'B', 'G', 'Z', 'I', 'K', 'R', 'V');
+        differentFilledSublist = new SingleLinkedListImpl<Character>('G', 'E', 'A', 'Z', 'R', 'P', 'V', 'N');
+        shortSublist = new SingleLinkedListImpl<Character>('Z', 'I', 'K');
+        emptySublist = new SingleLinkedListImpl<Character>();
+    }
+    @Test
+    public void emptyList(){
+        assertEquals(0, emptyList.isSubList(emptySublist));
+        assertEquals(-1, emptyList.isSubList(equalFilledSublist));
+    }
+    @Test
+    public void filledList(){
+        assertEquals(0, filledList.isSubList(emptySublist));
+        assertEquals(1, filledList.isSubList(equalFilledSublist));
+        assertEquals(-1, filledList.isSubList(differentFilledSublist));
+        assertEquals(4, filledList.isSubList(shortSublist));
+    }
+
+
+
+}
