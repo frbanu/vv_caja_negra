d1506dd96a869149ddbc611c4631a9b276361da1
Alberto <you@example.com>
2021-11-15 12:14:06 +0100

    Implementadas pruebas de isEmpty

--- src/test/java/com/practica/cajanegra/TestIsEmpty.java
+++ src/test/java/com/practica/cajanegra/TestIsEmpty.java
@@ -7,17 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestIsEmpty {
    @Test
-    void isEmptyCP1(){
+    void testIsEmptyCP1(){
        SingleLinkedListImpl listaVacia = new SingleLinkedListImpl();
        assertEquals(true,listaVacia.isEmpty());
    }
    @Test
-    void isEmptyCP2(){
+    void TestIsEmptyCP2(){
        SingleLinkedListImpl lista1Elemento = new SingleLinkedListImpl('A');
        assertEquals(false,lista1Elemento.isEmpty());
    }
    @Test
-    void isEmptyCP3(){
+    void TestIsEmptyCP3(){
        SingleLinkedListImpl listaElementos = new SingleLinkedListImpl('A','B','C');
        assertEquals(false,listaElementos.isEmpty());
    }
