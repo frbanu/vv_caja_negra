35016fd716af0e76cd3dd97268d173ce67405c5d
frbanu <65568086+frbanu@users.noreply.github.com>
2021-11-22 12:33:55 +0100

    nueva lista en el setUp()
    Se ha modificado el código actualizando la lista sobre la cual se hacen las pruebas.

--- src/test/java/com/practica/cajanegra/removeLastTest.java
+++ src/test/java/com/practica/cajanegra/removeLastTest.java
@@ -16,9 +16,8 @@ public class removeLastTest {
 
     @BeforeAll
     public static void setUp(){
-        miLista = new SingleLinkedListImpl<Character>('A','B','X','Z','L','L','A','H');
+        miLista = new SingleLinkedListImpl<Character>('A','B','C','F','L','L','Y','Z');
         listaVacia = new SingleLinkedListImpl<Character>();
-        Character[] casosPruebaInvalidos = {'@','['};
     }
 
     @DisplayName("Caso de prueba con lista vacía")
@@ -33,57 +32,76 @@ public class removeLastTest {
     @Test
     public void removeLastCP2(){
         Assertions.assertThrows(NoSuchElementException.class, () ->{
-            miLista.removeLast('C');
+            miLista.removeLast('G');
         });
     }
 
     @DisplayName("Caso de prueba 3, Eliminar el primer elemento de la lista")
     @Test
     public void removeLastCP3() throws EmptyCollectionException {
-        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('B','X','Z','L','L','A','H');
+        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('B','C','F','L','L','Y','Z');
         miLista.removeLast('A');
-        Assert.assertEquals(listaEsperada,miLista);
+        Assert.assertEquals(listaEsperada.toString(),miLista.toString());
     }
 
     @DisplayName("Caso de prueba 4, Eliminar el segundo elemento de la lista")
     @Test
     public void removeLastCP4() throws EmptyCollectionException{
-        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('A','X','Z','L','L','A','H');
+        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('A','C','F','L','L','Y',
+                'Z');
         miLista.removeLast('B');
+        System.out.println(listaEsperada);
         Assert.assertEquals(listaEsperada.toString(),miLista.toString());
     }
 
     @DisplayName("Caso de prueba 5, eliminar elemento medio de la lista")
     @Test
     public void removeLastCP5() throws EmptyCollectionException{
-        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('A','B','X','L','L','A','H');
-        miLista.removeLast('Z');
+        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('A','B','C','L','L','Y',
+                'Z');
+        miLista.removeLast('F');
         Assert.assertEquals(listaEsperada.toString(),miLista.toString());
     }
 
     @DisplayName("Caso de prueba 6, eliminamos el penúltimo elemento de la lista")
     @Test
     public void removeLastCP6() throws EmptyCollectionException{
-        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('A','B','X','Z','L','L','H');
-        miLista.removeLast('A');
+        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('A','B','C','F','L','L',
+                'Z');
+        miLista.removeLast('Y');
         Assert.assertEquals(listaEsperada.toString(),miLista.toString());
     }
 
     @DisplayName("Caso de prueba 7, eliminamos el último elemento de la lista")
     @Test
     public void removeLastCP7() throws EmptyCollectionException{
-        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('A','B','X','Z','L','L','A');
-        miLista.removeLast('H');
+        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<Character>('A','B','C','F','L','L','Y');
+        miLista.removeLast('Z');
         Assert.assertEquals(listaEsperada.toString(),miLista.toString());
     }
 
     @DisplayName("Caso de prueba 8, pasar como argumento un valor invalido")
     @Test
-    public void removeLastCP8() throws EmptyCollectionException{
-        SingleLinkedListImpl<Character> listaInvalida = new SingleLinkedListImpl<>('@','[');
-        SingleLinkedListImpl<Character> listaEsperada = new SingleLinkedListImpl<>('[');
-        listaInvalida.removeLast('@');
-        Assert.assertEquals(listaEsperada,listaInvalida);
+    public void removeLastCP8(){
+        Assertions.assertThrows(Exception.class, () ->{
+            miLista.removeLast('@');
+        });
+    }
+    @DisplayName("Caso de prueba 9, pasar como argumento un valor invalido")
+    @Test
+    public void removeLastCP9(){
+        Assertions.assertThrows(Exception.class, () ->{
+            miLista.removeLast('[');
+        });
+    }
+
+
+   @DisplayName("Caso de prueba 10, pasar como argumento un valor entero")
+    @Test
+    public void removeLastCP10() {
+        Assertions.assertThrows(Exception.class, () ->{
+            miLista.removeLast(10);
+        });
     }
 
 
